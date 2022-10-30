e86fb9bcf60ef87a261d28806d022bc1a148a5c0
kodavath laxman nayak <88278338+Laksh5257@users.noreply.github.com>
2022-10-30 10:43:14 +0530

    development

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 print(“Hello KodavathLaxmanNayak :D”)
 =======
 print(‘Hello world :D’)
 >>>>>>> main
+=======
+print(“Hello KodavathLaxmanNayak :D”)
+>>>>>>> Stashed changes
